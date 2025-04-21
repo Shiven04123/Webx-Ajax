d767f802661bc97a4fe56f424ccffe509d828626
Shiven Bansal <shivenapj@gmail.com>
2025-04-21 19:19:50 +0530

    added ajax in this experiment

--- server.js
+++ server.js
@@ -2,11 +2,17 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
+const path = require("path");
 
-const registeredUsers = ["john123", "alice456"]; // Simulate existing users
+
+const registeredUsers = ["shiven", "shivenbansal"]; // Simulate existing users
 
 app.use(bodyParser.json());
 app.use(express.static(__dirname));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "registration.html")); // or whatever your file is named
+});
 
 app.post("/check-username", (req, res) => {
   const { username } = req.body;
